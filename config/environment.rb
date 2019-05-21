181becbfc82915c52f799c6a64136a32793d7023
Anthony Rurup <tcrurup@gmail.com>
2019-05-21 04:10:42 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -9,5 +9,9 @@ require 'bundler/setup'
 Bundler.require
 
 
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
 require_relative "../artist.rb"
